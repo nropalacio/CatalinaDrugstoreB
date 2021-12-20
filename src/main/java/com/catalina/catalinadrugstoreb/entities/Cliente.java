fe6fd2913cbd9c65ffb275f5ebb30746ead00827
nropalacio <nrodrigo.palaciov@gmail.com>
2021-12-20 10:35:35 -0600

    primer commit

---
+++ src/main/java/com/catalina/catalinadrugstoreb/entities/Cliente.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.catalina.catalinadrugstoreb.entities;
+
+/**
+ *
+ * @author Nelson Palacios
+ */
+public class Cliente {
+
+    private Integer id;
+    private String name;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
